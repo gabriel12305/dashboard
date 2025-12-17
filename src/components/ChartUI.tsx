d19dbe5466c3662c313ae340b87d04813761a7ff
Gabriel Peláez <116526182+gabriel12305@users.noreply.github.com>
2025-12-17 04:08:14 +0000

    .

--- src/components/ChartUI.tsx
+++ src/components/ChartUI.tsx
@@ -1,10 +1,6 @@
 import { LineChart } from '@mui/x-charts/LineChart';
 import Typography from '@mui/material/Typography';
 
-const arrValues1 = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const arrValues2 = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const arrLabels = ['A','B','C','D','E','F','G'];
-
 interface IndicatorUIProps {
     value1?: string;
     value2?: string;
